66f63ed288e7de1dbd6e14677f7399dacc82f952
Arpan Thomas <66914302+Arpanthomas1121@users.noreply.github.com>
2022-05-22 18:20:39 +0530

    Update tiles.php
    updated php

--- assets/tiles.php
+++ assets/tiles.php
@@ -8,12 +8,32 @@
         <link rel="stylesheet" href="tilesCss.css">
     </head>
     <body>
-        <div class="container">
+           <?php
+        session_start();
+        $id=$_GET['id'];
+        ?>
+       <form method="POST" action="tiles.php">
+       <div class="container">
             <button class="option" name="subject" type="submit" value="1"></button>
             <button class="option" name="subject" type="submit" value="2"></button>
             <button class="option" name="subject" type="submit" value="3"></button>
-            <button class="option" name="subject" type="submit" value="4"></button>
-            
-</div>
+            <button class="option" name="subject" type="submit" value="4"></button>      
+        </div>
+       </form>
+
+       <?php
+       if(isset($_POST['subject']))
+       {
+        $icaj = $_POST['subject'];
+        $sql = "SELECT *  FROM `sensdata` WHERE `icaj` = $icaj AND Email=$id" or die("Query Unsuccessfull:" . mysqli_error($connection));
+        if ($query_run) {
+            header("Location: realIndex.php?id=".$_SESSION["id"]);
+        } else {
+            header("Location: fake.php?id=".$_SESSION["id"]);
+            session_destroy();
+         }
+       }
+
+?>
     </body>
 </html>
