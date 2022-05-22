ff629774658df37d0ad786e2b5c4c95fd98feab9
Arpan Thomas <66914302+Arpanthomas1121@users.noreply.github.com>
2022-05-22 16:18:09 +0530

    Update index.php
    updates index

--- assets/index.php
+++ assets/index.php
@@ -1,16 +1,23 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>CyberV</title>
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-       <div class="loader">
-            <span></span>
-            <span></span>
-            <span></span>
-            <h2>Loading...</h2>
-       </div>
-    </body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="script-src 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' ;" >
+    <link rel="stylesheet" href="style copy.css">
+
+    <title>CyberV</title>
+  </head>
+  <body>
+  <div class="container">
+    <div class="box">
+      <h1>Welcome</h1>
+      <h2>to the <span>CyberV</span> WebApp</h2>
+      <a class="button" href="login.php">Login</a>
+      <a class="button" href="register.php">Register</a>
+    </div>
+  </div>
+  </div>
+<script></script>  
+</body>
 </html>
