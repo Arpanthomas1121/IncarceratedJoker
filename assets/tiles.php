669f5e9288d4208a69388f4238ab84fe490930bc
Arpan Thomas <66914302+Arpanthomas1121@users.noreply.github.com>
2022-06-02 02:42:40 +0530

    Update tiles.php
    Tile authentication bug fix and beautify

--- assets/tiles.php
+++ assets/tiles.php
@@ -1,39 +1,50 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>CyberV</title>
-        <link rel="stylesheet" href="tilesCss.css">
-    </head>
-    <body>
-           <?php
-        session_start();
-        $id=$_GET['id'];
-        ?>
-       <form method="POST" action="tiles.php">
-       <div class="container">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>CyberV</title>
+    <link rel="stylesheet" href="tilesCss.css">
+</head>
+
+<body>
+    <?php
+    include "connection.php";
+    session_start();
+    if (isset($_GET)) {
+        $id = $_GET["id"];
+    } else {
+        echo 'fcuked up life!! fcukedup website!!';
+        echo 'No issues register again!!';
+        header("Location: login.php");
+    }
+
+    ?>
+    <form method="POST" action="">
+        <div class="container">
             <button class="option" name="subject" type="submit" value="1"></button>
             <button class="option" name="subject" type="submit" value="2"></button>
             <button class="option" name="subject" type="submit" value="3"></button>
-            <button class="option" name="subject" type="submit" value="4"></button>      
+            <button class="option" name="subject" type="submit" value="4"></button>
         </div>
-       </form>
-
-       <?php
-       if(isset($_POST['subject']))
-       {
+    </form>
+    <?php
+    if (isset($_POST['subject'])) {
         $icaj = $_POST['subject'];
-        $sql = "SELECT *  FROM `sensdata` WHERE `icaj` = $icaj AND Email=$id" or die("Query Unsuccessfull:" . mysqli_error($connection));
-        if ($query_run) {
-            header("Location: realIndex.php?id=".$_SESSION["id"]);
-        } else {
-            header("Location: fake.php?id=".$_SESSION["id"]);
-            session_destroy();
-         }
-       }
 
-?>
-    </body>
+        $query = mysqli_query($connection, "SELECT `icaj` FROM `sensdata` WHERE `Email`= '$id' AND `icaj`='$icaj';") or die("Query Unsuccessfull:" . mysqli_error($connection));
+        $num_rows = mysqli_num_rows($query);
+        $row = mysqli_fetch_array($query);
+        if ($num_rows == 1) {
+            header("Location: realIndex.php");
+        } else {
+            header("Location: fake.php");
+            session_destroy();
+        }
+    }
+    ?>
+</body>
+
 </html>
